f742f880b4ee38d1bdf9d62152147bb9516c773f
brizzinck <87083857+brizzinck@users.noreply.github.com>
2024-11-24 00:49:36 +0000

    feat: change validation password

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -15,19 +15,29 @@ export const validateField = (name, value, formData = {}) => {
             }
             break;
         case "password":
-            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
+            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,20}$/;
             const commonPasswords = ["123456", "password", "qwerty", "12345678"];
+            const onlyLettersRegex = /^[A-Za-z]+$/;
+            const onlyNumbersRegex = /^\d+$/;
+            const onlySymbolsRegex = /^[@$!%*?&#]+$/;
 
             if (!passwordRegex.test(value)) {
-                error = "Пароль повинен містити мінімум 8 символів, тільки латинські букви, одну велику літеру, одну цифру та один спеціальний символ.";
+                error = "Пароль повинен містити від 8 до 20 символів, тільки латинські букви, хоча б одну велику літеру, одну цифру та один спеціальний символ.";
             } else if (commonPasswords.includes(value.toLowerCase())) {
                 error = "Пароль надто простий. Оберіть більш надійний пароль.";
             } else if (/^(.)\1+$/.test(value)) {
                 error = "Пароль не повинен складатися з повторюваних символів.";
             } else if (value.length > 20) {
                 error = "Пароль не повинен бути довшим за 20 символів.";
+            } else if (onlyLettersRegex.test(value)) {
+                error = "Пароль не повинен складатися лише з літер.";
+            } else if (onlyNumbersRegex.test(value)) {
+                error = "Пароль не повинен складатися лише з цифр.";
+            } else if (onlySymbolsRegex.test(value)) {
+                error = "Пароль не повинен складатися лише з спеціальних символів.";
             }
             break;
+
         case "phone_number":
             const phoneRegex = /^\+380\d{9}$/;
             if (!phoneRegex.test(value)) {
