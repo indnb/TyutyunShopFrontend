eba16c74949978a9574040d23d72918d7605cd71
Vlad Lavrishko <87083857+brizzinck@users.noreply.github.com>
2024-11-25 11:06:28 +0200

    feat: add login and name validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -3,9 +3,9 @@ export const validateField = (name, value, formData = {}) => {
 
     switch (name) {
         case "username":
-            const usernameRegex = /^[A-Za-z]{3,}$/;
+            const usernameRegex = /^[A-Za-z][A-Za-z0-9]{2,19}$/;
             if (!usernameRegex.test(value)) {
-                error = "Логін повинен містити лише англійські літери та бути не коротшим за 3 символи.";
+                error = "Логін повинен починатися з літери, містити лише англійські літери та цифри, та бути не коротшим за 3 і не довшим за 20 символів.";
             }
             break;
         case "email":
@@ -15,29 +15,19 @@ export const validateField = (name, value, formData = {}) => {
             }
             break;
         case "password":
-            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,20}$/;
+            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
             const commonPasswords = ["123456", "password", "qwerty", "12345678"];
-            const onlyLettersRegex = /^[A-Za-z]+$/;
-            const onlyNumbersRegex = /^\d+$/;
-            const onlySymbolsRegex = /^[@$!%*?&#]+$/;
 
             if (!passwordRegex.test(value)) {
-                error = "Пароль повинен містити від 8 до 20 символів, тільки латинські букви, хоча б одну велику літеру, одну цифру та один спеціальний символ.";
+                error = "Пароль повинен містити мінімум 8 символів, тільки латинські букви, одну велику літеру, одну цифру та один спеціальний символ.";
             } else if (commonPasswords.includes(value.toLowerCase())) {
                 error = "Пароль надто простий. Оберіть більш надійний пароль.";
             } else if (/^(.)\1+$/.test(value)) {
                 error = "Пароль не повинен складатися з повторюваних символів.";
             } else if (value.length > 20) {
                 error = "Пароль не повинен бути довшим за 20 символів.";
-            } else if (onlyLettersRegex.test(value)) {
-                error = "Пароль не повинен складатися лише з літер.";
-            } else if (onlyNumbersRegex.test(value)) {
-                error = "Пароль не повинен складатися лише з цифр.";
-            } else if (onlySymbolsRegex.test(value)) {
-                error = "Пароль не повинен складатися лише з спеціальних символів.";
             }
             break;
-
         case "phone_number":
             const phoneRegex = /^\+380\d{9}$/;
             if (!phoneRegex.test(value)) {
@@ -51,6 +41,8 @@ export const validateField = (name, value, formData = {}) => {
                 error = "Це поле обов'язкове.";
             } else if (!nameRegex.test(value)) {
                 error = "Ім'я або прізвище повинні містити лише літери та бути не коротшими за 2 символи.";
+            } else if (value.length > 20) {
+                error = "Ім'я або прізвище повинні бути не довшими за 20 символів.";
             }
             break;
         case "confirm_password":
